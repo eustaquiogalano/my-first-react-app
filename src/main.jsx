7f52c531632f12d9537bca8f3b41eaf4aef04dcb
eustaquio <eustaquio.galano@gmail.com>
2025-11-22 00:30:30 +0800

    import and use App2 component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { default as Pagbati, NamedSample, EmailInput } from "./Greeting.jsx";
+import {
+  default as Pagbati,
+  NamedSample,
+  EmailInput,
+  App2,
+} from "./Greeting.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Pagbati />
-    <NamedSample />
-    <EmailInput />
+    <App2 />
   </StrictMode>
 );
