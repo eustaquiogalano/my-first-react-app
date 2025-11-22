4331945e92bfbab2cefa6ebd604df425d8c14e54
eustaquio <eustaquio.galano@gmail.com>
2025-11-22 22:26:52 +0800

    import and use BookList component

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,12 @@ import {
   App3,
 } from "./Greeting.jsx";
 import App from "./App.jsx";
+import { BookList } from "./gospelList.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App2 name="Jesus Christ" father="God" />
     <App3 />
+    <BookList />
   </StrictMode>
 );
