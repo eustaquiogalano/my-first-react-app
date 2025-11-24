c199e2722c991634e066096b387257d683d0fabb
eustaquio <eustaquio.galano@gmail.com>
2025-11-25 01:08:04 +0800

    import and use practice components

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,14 @@ import {
 } from "./Greeting.jsx";
 import App from "./App.jsx";
 import { BookList } from "./gospelList.jsx";
+import { ListOfProfessionals, ScientistList } from "./RenderList.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App2 name="Jesus Christ" father="God" />
     <App3 />
     <BookList />
+    <ListOfProfessionals />
+    <ScientistList />
   </StrictMode>
 );
