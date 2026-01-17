2b80c0ae797dc224151428053c6736127ce6e430
eustaquio <eustaquio.galano@gmail.com>
2026-01-17 12:07:54 +0800

    react testing library practice

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,54 @@
+import { describe, it, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import App from "./../App";
+import MouseCirclePointer from "../components/MouseCirclePointer/MouseCirclePointer";
+
+// describe("something truthy and falsy", () => {
+//   it("true to be true", () => {
+//     expect(true).toBe(true);
+//   });
+
+//   it("false to be false", () => {
+//     expect(false).toBe(false);
+//   });
+// });
+
+// describe("App", () => {
+//   it("render App", () => {
+//     render(<App />);
+
+//     // screen.debug();
+//   });
+
+//   it("render mcp", () => {
+//     render(<MouseCirclePointer />);
+
+//     const checkbox = screen.getByRole("checkbox", {
+//       name: "The dot is allowed to move",
+//     });
+
+//     expect(checkbox).toBeInTheDocument();
+//   });
+// });
+
+test("render label", () => {
+  // render the component
+  render(<MouseCirclePointer />);
+
+  // 1
+  // dislay dom in the console
+  // screen.debug();
+
+  // assertions
+  // expect(screen.getByText("The dot is allowed to move")).toBeInTheDocument();
+
+  // 2
+  const listElement = screen.getByRole("list");
+  const listItems = screen.getAllByRole("listitem");
+
+  // assertions again
+  expect(listElement).toBeInTheDocument();
+  expect(listElement).toHaveClass("apostles");
+  expect(listItems.length).toEqual(4);
+});
