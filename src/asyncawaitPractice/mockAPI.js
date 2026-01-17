8691d5dcc84bb826d24ab5249685985a6e854f8b
eustaquio <eustaquio.galano@gmail.com>
2026-01-17 12:26:16 +0800

    chore: add mock database and API

---
+++ src/asyncawaitPractice/mockAPI.js
@@ -0,0 +1,12 @@
+import mockDatabase from "./mockDB";
+
+// simulate network delay
+function mockDelay() {
+  return new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
+export async function getUsers() {
+  await mockDelay(); // network delay
+
+  return mockDatabase;
+}
---
+++ src/asyncawaitPractice/mockDB.js
@@ -0,0 +1,96 @@
+const mockDatabase = [
+  {
+    userType: "student",
+    username: "joris.arancon",
+    password: "jorispogi",
+    email: "joris.arancon@gmail.com",
+    name: "Jor",
+    idNumber: "1X0COL-XXXXXX",
+    yearLevel: 3,
+    isLoggedIn: false,
+    requestedDocuments: [
+      {
+        id: crypto.randomUUID(),
+        document: "Diploma",
+        purpose: "Job Application",
+        date: "01/01/2026",
+        status: "PENDING",
+      },
+      {
+        id: crypto.randomUUID(),
+        document: "TOR",
+        purpose: "Scholarship",
+        date: "01/01/2026",
+        status: "PENDING",
+      },
+    ],
+  },
+  {
+    userType: "student",
+    username: "dexter.aras",
+    password: "pagdextersweetlover",
+    email: "dexter.aras@gmail.com",
+    name: "Dex",
+    idNumber: "2X0COL-XXXXXX",
+    yearLevel: 3,
+    isLoggedIn: false,
+    requestedDocuments: [
+      {
+        id: crypto.randomUUID(),
+        document: "CTC",
+        purpose: "Scholarship",
+        date: "01/01/2026",
+        status: "PENDING",
+      },
+      {
+        id: crypto.randomUUID(),
+        document: "Form 138",
+        purpose: "Scholarship",
+        date: "01/01/2026",
+        status: "PENDING",
+      },
+      {
+        id: crypto.randomUUID(),
+        document: "TOR",
+        purpose: "Scholarship",
+        date: "01/01/2026",
+        status: "PENDING",
+      },
+    ],
+  },
+  {
+    userType: "student",
+    username: "jannel.gualva",
+    password: "jannely",
+    email: "jannel.gualva@gmail.com",
+    name: "Ja",
+    idNumber: "2X0COL-XXXXXX",
+    yearLevel: 3,
+    isLoggedIn: false,
+    requestedDocuments: [
+      {
+        id: crypto.randomUUID(),
+        document: "CTC",
+        purpose: "Scholarship",
+        date: "01/01/2026",
+        status: "PENDING",
+      },
+      {
+        id: crypto.randomUUID(),
+        document: "Form 138",
+        purpose: "Scholarship",
+        date: "01/01/2026",
+        status: "PENDING",
+      },
+      {
+        id: crypto.randomUUID(),
+        document: "TOR",
+        purpose: "Scholarship",
+        date: "01/01/2026",
+        status: "PENDING",
+      },
+    ],
+  },
+];
+
+export default mockDatabase;
