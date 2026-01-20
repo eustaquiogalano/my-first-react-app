9cd4f80432060760b3c3404f2b130e0390af6473
eustaquio <eustaquio.galano@gmail.com>
2026-01-21 00:46:43 +0800

    feat: add deleteUser and updateUser functions

--- src/asyncawaitPractice/mockAPI.js
+++ src/asyncawaitPractice/mockAPI.js
@@ -15,5 +15,26 @@ export async function addUser(user) {
   await mockDelay();
 
   mockDatabase.push(user);
+  return user;
+}
+
+export async function deleteUser(name) {
+  await mockDelay();
+
+  mockDatabase = mockDatabase.filter((user) => user.name !== name);
+
+  return mockDatabase;
+}
+
+export async function updateUser(selectedUser, updatedUser) {
+  await mockDelay();
+
+  mockDatabase = mockDatabase.map((user) => {
+    if (user.name === selectedUser.name) {
+      return updatedUser;
+    }
+    return user;
+  });
+
   return mockDatabase;
 }
