2c5bb356d25cb4c7d0fd56a2c9d3fd8ee5bd8fab
eustaquio <eustaquio.galano@gmail.com>
2026-01-23 23:28:51 +0800

    chore: remove unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode, React } from "react";
 import { createRoot, ReactDOM } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Gallery from "./components/gallery-exercise/GalleryApp.jsx";
-import Form from "./components/gallery-exercise/FormExercise.jsx";
-import { PlaceTree } from "./components/gallery-exercise/PlaceTree.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
