d71c2f090f18df50f81b3781ff2f9c74fc7b35ec
eustaquio <eustaquio.galano@gmail.com>
2026-01-25 01:28:06 +0800

    feat: add DefaultProfilePage component

---
+++ src/components/ClientSideRouting/ProfileChildrenComponents/DefaultProfilePage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router";
+
+function DefaultProfilePage() {
+  return (
+    <div>
+      <h2>Default Profile</h2>
+    </div>
+  );
+}
+
+export default DefaultProfilePage;
