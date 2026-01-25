91c4990558bc23b5f25c378a442d470e203f3461
eustaquio <eustaquio.galano@gmail.com>
2026-01-25 12:26:03 +0800

    feat: add ErrorPage component

---
+++ src/components/ClientSideRouting/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router";
+
+function ErrorPage() {
+  return (
+    <div>
+      <h2>This nothing here</h2>
+      <Link to="/">Click here to go back to main page.</Link>
+    </div>
+  );
+}
+export default ErrorPage;
