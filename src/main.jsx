d2909131ccd7fa3e17c7b81d20e18d88c23b7c49
eustaquio <eustaquio.galano@gmail.com>
2026-01-25 09:42:08 +0800

    feat: setup reouting for core pages
    - Add react-router configuration
    - Profile, CSRApp, About, DefaultProfilePage, and Freinds routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode, React } from "react";
 import { createRoot, ReactDOM } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
-import App from "./App.jsx";
+import CSRApp from "./components/ClientSideRouting/CSRApp.jsx";
+import Profile from "./components/ClientSideRouting/Profile.jsx";
+import Friends from "./components/ClientSideRouting/ProfileChildrenComponents/Friends.jsx";
+import About from "./components/ClientSideRouting/ProfileChildrenComponents/About.jsx";
+import DefaultProfilePage from "./components/ClientSideRouting/ProfileChildrenComponents/DefaultProfilePage.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <CSRApp /> },
+  {
+    path: "profile",
+    element: <Profile />,
+    children: [
+      { index: true, element: <DefaultProfilePage /> },
+      { path: "friends", element: <Friends /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
