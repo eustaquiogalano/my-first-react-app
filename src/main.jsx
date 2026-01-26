e2abc10e89e05a2e7a8aadd02d52287b6ad1aabf
eustaquio <eustaquio.galano@gmail.com>
2026-01-26 15:51:49 +0800

    feat: add contact details handler

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,19 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import "././components/CSRContactsPractice/ContactsApp.css";
 import ContactsApp from "./components/CSRContactsPractice/ContactsApp";
 import ContactsErrorPage from "./components/CSRContactsPractice/ContactsErrorPage";
+import ContactCard from "./components/CSRContactsPractice/ContactsCard";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <ContactsApp />,
     errorElement: <ContactsErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactID",
+        element: <ContactCard />,
+      },
+    ],
   },
 ]);
 
