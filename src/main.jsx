e66a796e362a793253a811ef0da278174f815688
eustaquio <eustaquio.galano@gmail.com>
2026-01-26 12:21:43 +0800

    chore: remove unused import lines

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import { StrictMode, React } from "react";
 import { createRoot, ReactDOM } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
-import CSRApp from "./components/ClientSideRouting/CSRApp.jsx";
-import Profile from "./components/ClientSideRouting/Profile.jsx";
-import Friends from "./components/ClientSideRouting/ProfileChildrenComponents/Friends.jsx";
-import About from "./components/ClientSideRouting/ProfileChildrenComponents/About.jsx";
-import DefaultProfilePage from "./components/ClientSideRouting/ProfileChildrenComponents/DefaultProfilePage.jsx";
-import ErrorPage from "./components/ClientSideRouting/ErrorPage.jsx";
-import routes from "./components/ClientSideRouting/routes.jsx";
 
 const router = createBrowserRouter(routes);
 
